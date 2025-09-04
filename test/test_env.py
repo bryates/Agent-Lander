926f0f285f8388bb27f5aee0d4d4ef065d17a107
Brent Yates <yates.313@osu.edu>
2025-09-04 18:29:31 -0400

    Manual fix

--- test/test_env.py
+++ test/test_env.py
@@ -2,6 +2,9 @@
 LunarLander-v3 environment using PyTorch and Gymnasium.'''
 
 import gymnasium as gym
+import Box2D
+Box2D.b2.SWIG_PyPacked.__module__ = "Box2D"
+Box2D.b2.SWIG_PyObject.__module__ = "Box2D"
 
 ENV_NAME = 'LunarLander-v3'  # Name of the Gym environment
 env = gym.make(ENV_NAME)
